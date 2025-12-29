7107cea3d244d56ce82ac488ea0c4db44fb2861c
Royroad <imyeongrok@woowayouths.com>
2025-12-29 16:27:15 +0900

    헤더와 푸터 제거: 최소한의 안내 페이지만 표시

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import ShutdownPage from './pages/ShutdownPage';
 
 /**
  * 메인 App 컴포넌트
- * 서비스 종료로 인해 모든 라우트를 종료 안내 페이지로 리다이렉트
+ * 간소화된 안내 페이지만 표시
  */
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* 공통 헤더 */}
-        <Header />
-
-        {/* 모든 라우트를 종료 페이지로 리다이렉트 */}
+        {/* 모든 라우트를 안내 페이지로 리다이렉트 */}
         <Routes>
           <Route path="/" element={<ShutdownPage />} />
           <Route path="/suggestions" element={<Navigate to="/" replace />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-
-        {/* 공통 푸터 */}
-        <Footer />
       </div>
     </Router>
   );
